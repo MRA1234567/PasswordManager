3d6cd1f7f8ac70f365b84ab0cf419567d6e23fbf
Mehtab Ali <146185448+MRA1234567@users.noreply.github.com>
2025-02-01 14:26:02 +0000

    Added bootsrap, navbar and sidebar

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Enable serving static files from the wwwroot folder
-app.UseDefaultFiles(); // Enables serving index.html as the default file
-app.UseStaticFiles();  // Enables serving static files like CSS, JS, images, etc.
+app.UseDefaultFiles();
+app.UseStaticFiles(); 
 
-// Start the app
 app.Run();
