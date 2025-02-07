aa0dc770d00ad20571523c30d0586559c7f3327a
Mehtab Ali <146185448+MRA1234567@users.noreply.github.com>
2025-02-07 14:22:11 +0000

    adding Password folders

---
+++ wwwroot/js/spa.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#PassFolder1').click(function () {
+        $('#page-content-wrapper').load('PassFolder1.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#PassFolder2').click(function () {
+        $('#page-content-wrapper').load('PassFolder2.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#PassFolder3').click(function () {
+        $('#page-content-wrapper').load('PassFolder3.html');
+    })
+});
